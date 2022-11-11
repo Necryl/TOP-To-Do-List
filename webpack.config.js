e65acd8e5b97eaee70258f665b441e31e6eb5707
Necryl <necrylthewayfarer@gmail.com>
2022-11-11 09:20:46 +0530

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, "src/scripts/index.js"),
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.js",
+        assetModuleFilename: '[name][ext]',
+        clean: true,
+    },
+    devtool: "inline-source-map",
+    devServer: {
+        static: path.resolve(__dirname, "dist"),
+        port: 8080,
+        open: true,
+        hot: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(svg|ico|png|gif|jpg|jpeg|webp)$/,
+                type: "asset/resource"
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "To-Do List: A TOP Project",
+            filename: "index.html",
+        })
+    ]
+};
