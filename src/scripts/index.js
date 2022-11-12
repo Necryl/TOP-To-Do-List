755707e21194ecbf213876bd1b8e74294c3beae1
Necryl <necrylthewayfarer@gmail.com>
2022-11-12 09:06:44 +0530

    split main.css into multiple files

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,6 @@
+// importing css
 import mainStyles from "./../styles/main.css"
+import menuStyles from "./../styles/menu.css"
+import listViewtyles from "./../styles/listView.css"
+import contentViewStyles from "./../styles/contentView.css"
+
