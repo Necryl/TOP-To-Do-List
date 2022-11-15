70e2082a18b5e3cd8ed77bc6f2beb5651bf007d4
Necryl <necrylthewayfarer@gmail.com>
2022-11-15 16:35:55 +0530

    add navigation buttons in html and add some beginning code for getting viewport aspect ratio

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,3 +4,22 @@ import menuStyles from "./../styles/menu.css"
 import listViewtyles from "./../styles/listView.css"
 import contentViewStyles from "./../styles/contentView.css"
 
+// elements
+const rootElement = document.querySelector(':root');
+
+// state variables
+let displayState; // possible states: landscape, square, portrait
+
+
+// events
+window.onresize = updateDisplayState;
+
+function updateDisplayState () {
+    let height = rootElement.clientHeight;
+    let width = rootElement.clientWidth;
+    console.log(`Height: ${height}, Width: ${width}, Aspect ratio: ${width/height}`);
+}
+
+// on start
+updateDisplayState();
+
