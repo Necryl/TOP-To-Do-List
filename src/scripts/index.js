a5a99f1d3861bb239ab358aa4caebf8734b92a7e
Necryl <necrylthewayfarer@gmail.com>
2022-11-15 17:27:14 +0530

    polish the ui changes for each displayState. Show/hide buttons and titles

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,10 +9,13 @@ const rootElement = document.querySelector(':root');
 const closeMenuBtnElement = document.querySelector('#close-menu');
 const openMenuBtnElement = document.querySelector('#open-menu');
 const returnBtnElement = document.querySelector('#return');
-const navBtns = [closeMenuBtnElement, openMenuBtnElement, returnBtnElement];
+const navBtnElements = [closeMenuBtnElement, openMenuBtnElement, returnBtnElement];
 const menuElement = document.querySelector('.menu');
 const listViewElement = document.querySelector('.listView');
 const contentViewElement = document.querySelector('.contentView');
+const menuTitleElement = document.querySelector('.menu .title');
+const listViewTitleElement = document.querySelector('.listView .title');
+
 
 
 // state variables
@@ -28,12 +31,13 @@ function updateDisplayState () {
     let height = rootElement.clientHeight;
     let width = rootElement.clientWidth;
     let ratio = width/height;
+    let menuWidth = menuElement.clientWidth;
 
     let result;
 
     if (ratio < 0.72) {
         result = 'single';
-    } else if (ratio < 1.178) {
+    } else if (ratio < 1.178 || width/menuWidth < 3) {
         result = 'double';
     } else {
         result = 'triple';
@@ -45,12 +49,16 @@ function updateDisplayState () {
 
 function updateUiMode () {
     if (displayState === 'triple') {
-        navBtns.forEach((button) => {button.style.display = 'none'});
+        navBtnElements.forEach((button) => {button.style.display = 'none'});
         
         menuElement.style.display = 'grid';
         listViewElement.style.display = 'grid';
         contentViewElement.style.display = 'grid';
-        contentViewElement.style.display = 'grid';
+        
+        rootElement.style.setProperty('--colMinusOne', 2);
+
+        menuTitleElement.style.display = 'initial';
+        listViewTitleElement.style.display = 'none';
     } else if (displayState === 'double') {
         closeMenuBtnElement.style.display = 'initial';
         openMenuBtnElement.style.display = 'initial';
@@ -59,14 +67,23 @@ function updateUiMode () {
         menuElement.style.display = 'none';
         listViewElement.style.display = 'grid';
         contentViewElement.style.display = 'grid';
+
+        rootElement.style.setProperty('--colMinusOne', 2);
+        
+        menuTitleElement.style.display = 'none';
+        listViewTitleElement.style.display = 'initial';
     } else {
-        navBtns.forEach(button => {
+        navBtnElements.forEach(button => {
             button.style.display = 'initial';
         })
         menuElement.style.display = 'none';
         listViewElement.style.display = 'grid';
         contentViewElement.style.display = 'none';
 
+        rootElement.style.setProperty('--colMinusOne', 1);
+
+        menuTitleElement.style.display = 'none';
+        listViewTitleElement.style.display = 'initial';
     }
 }
 
