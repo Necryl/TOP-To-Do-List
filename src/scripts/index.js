aec6b5f7346f4f44a0729aece92a364bddac788d
Necryl <necrylthewayfarer@gmail.com>
2022-11-15 17:04:40 +0530

    show/hide different sections based on displayState

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,6 +10,9 @@ const closeMenuBtnElement = document.querySelector('#close-menu');
 const openMenuBtnElement = document.querySelector('#open-menu');
 const returnBtnElement = document.querySelector('#return');
 const navBtns = [closeMenuBtnElement, openMenuBtnElement, returnBtnElement];
+const menuElement = document.querySelector('.menu');
+const listViewElement = document.querySelector('.listView');
+const contentViewElement = document.querySelector('.contentView');
 
 
 // state variables
@@ -43,14 +46,27 @@ function updateDisplayState () {
 function updateUiMode () {
     if (displayState === 'triple') {
         navBtns.forEach((button) => {button.style.display = 'none'});
+        
+        menuElement.style.display = 'grid';
+        listViewElement.style.display = 'grid';
+        contentViewElement.style.display = 'grid';
+        contentViewElement.style.display = 'grid';
     } else if (displayState === 'double') {
         closeMenuBtnElement.style.display = 'initial';
         openMenuBtnElement.style.display = 'initial';
         returnBtnElement.style.display = 'none';
+
+        menuElement.style.display = 'none';
+        listViewElement.style.display = 'grid';
+        contentViewElement.style.display = 'grid';
     } else {
         navBtns.forEach(button => {
             button.style.display = 'initial';
         })
+        menuElement.style.display = 'none';
+        listViewElement.style.display = 'grid';
+        contentViewElement.style.display = 'none';
+
     }
 }
 
