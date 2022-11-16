3718125afc92fa76b911cdfa95b9713d7f6ceb31
Necryl <necrylthewayfarer@gmail.com>
2022-11-16 17:34:05 +0530

    make displayMode switching more accurate by checking for horizontal scrollbars

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,17 +10,16 @@ const bodyElement = document.querySelector('body');
 const closeMenuBtnElement = document.querySelector('#close-menu');
 const openMenuBtnElement = document.querySelector('#open-menu');
 const returnBtnElement = document.querySelector('#return');
-const navBtnElements = [closeMenuBtnElement, openMenuBtnElement, returnBtnElement];
 const menuContainerElement = document.querySelector('.menu-container');
 const menuElement = document.querySelector('.menu');
 const listViewElement = document.querySelector('.listView');
 const contentViewElement = document.querySelector('.contentView');
-const menuTitleElement = document.querySelector('.menu .title');
-const listViewTitleElement = document.querySelector('.listView .title');
+const listViewOptionsElement = document.querySelector('.listView .options');
 
 
 // state variables
 let displayState; // possible states: triple, double, single
+let timeWindow = false;
 
 // hardcoded data
 const elementsWhoseClassesReflectDisplayState = [bodyElement, menuElement, listViewElement, contentViewElement];
@@ -36,9 +35,8 @@ const UI = (() => {
     
         let result;
     
-        if (ratio < 0.72) {
-            result = 'single';
-        } else if (ratio < 1.178 || width/menuWidth < 3) {
+        if (width/menuWidth < 3) {
+            console.log('double it is');
             result = 'double';
         } else {
             result = 'triple';
@@ -49,12 +47,26 @@ const UI = (() => {
     }
     
     function updateDisplayMode () {
+        console.log('hahahahahha', displayState);
         addClasses(displayState, elementsWhoseClassesReflectDisplayState);
         possibleDisplayStates.forEach(className => {
             if (displayState !== className) {
                 removeClasses(className, elementsWhoseClassesReflectDisplayState);
             }
         })
+
+        let overflow = false;
+        [...getListViewItems(), listViewElement, listViewOptionsElement, contentViewElement].forEach(element => {
+            if (checkForScrollBars(element, 'horizontal') === true) {
+                overflow = true;
+            }
+        });
+        console.log('overflow = ', overflow);
+        if (overflow===true && displayState !== 'single') {
+            console.log('there is overflow', 'current displayState: '+displayState);
+            displayState = displayState === 'triple' ? 'double':'single';
+            setTimeout(()=>{updateDisplayMode();}, 10)
+        }
     }
     
     function hideMenu () {
@@ -63,19 +75,26 @@ const UI = (() => {
         })
     }
 
+    function getListViewItems () {
+        return [...document.querySelectorAll('.listView ul li')];
+    }
+
+    function checkForScrollBars (element, check='both') {
+        return check==='both' || check==='horizontal' ? element.scrollWidth > element.clientWidth ? true: check==='both' ? element.scrollHeight>element.clientHeight: false :element.scrollHeight > element.clientHeight;
+    }
+
+    function addClasses (className, elements=[]) {
+        elements.forEach(elem => {elem.classList.add(className);});
+    }
+    
+    function removeClasses (className, elements=[]) {
+        elements.forEach(elem => {elem.classList.remove(className);});
+    }
+
     return {
-        updateDisplayState, hideMenu,
+        updateDisplayState, hideMenu, checkForScrollBars,
     }
 })()
-
-// tool functions
-function addClasses (className, elements=[]) {
-    elements.forEach(elem => {elem.classList.add(className);});
-}
-
-function removeClasses (className, elements=[]) {
-    elements.forEach(elem => {elem.classList.remove(className);});
-}
 
 // events
 window.onresize = () => {UI.updateDisplayState();};
@@ -99,3 +118,7 @@ menuContainerElement.addEventListener('click', (event) => {
 
 // on start
 UI.updateDisplayState();
+
+// for testing
+// console.log('--------Testing--------');
+// console.log('----------End of Testing---------')
